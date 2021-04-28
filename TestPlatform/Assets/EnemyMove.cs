f0f7c0328efff04b6a6ac7e07c39ab6cfe18c9b7
Twirl1511 <Tiwlr1511@yandex.ru>
2021-04-28 18:46:15 +0300

    !F продолжаем делать домашку

---
+++ TestPlatform/Assets/EnemyMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private Transform tr;
+    private GameObject player;
+    private Transform playerTransform;
+    public float speed;
+    private void Start()
+    {
+        tr = GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = player.transform;
+    }
+    private void FixedUpdate()
+    {
+        Vector3 playerPos = playerTransform.position;
+        tr.position = Vector3.MoveTowards(tr.position, Vector3.Lerp(tr.position, playerPos, 0.1f), speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+    private void OnDisable()
+    {
+
+    }
+}
---
+++ TestPlatform/Assets/LookAtEnemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtEnemy : MonoBehaviour
+{
+    public static LookAtEnemy singleton;
+    private Transform tr;
+    private GameObject[] enemies;
+
+    public delegate void Delegate();
+    public static event Delegate myDelegate;
+    private int rand;
+    void Start()
+    {
+        if (singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        singleton = this;
+        tr = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FindNewClosestEnemy();
+        }
+        
+        LootAtEnemy();
+    }
+    
+
+    public void FindNewClosestEnemy()
+    {
+        myDelegate?.Invoke();
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        rand = Random.Range(0, enemies.Length);
+
+    }
+    public void LootAtEnemy()
+    {
+        
+        Vector3 direction = enemies[rand].transform.position - tr.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        tr.rotation = Quaternion.Lerp(tr.rotation, rotation, 5f * Time.deltaTime);
+    }
+
+}
